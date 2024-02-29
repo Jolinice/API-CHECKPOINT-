dccec6bf4237749745435f37c6e6339b6d8fbda1
SeyiBot <pricetwice007gmail.com>
2024-02-29 15:49:41 +0100

    api checkpoint

---
+++ my-api/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserList from './Components/userList';
+import './App.css'; // You can create and import your own styles
+
+function App() {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
